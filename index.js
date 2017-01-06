9b56334930e4224c7f50b387e534125ed2ce5eb7
albert_chang <albert_chang@symantec.com>
2017-01-06 13:25:45 -0800

    Fix

--- index.js
+++ index.js
@@ -3,28 +3,26 @@
 const Webpack = require('webpack');
 const WebpackDevMiddleware = require('webpack-dev-middleware');
 
-exports.register = (server, { config }, next) => {
-  if (!config) {
-    return reply(err);
-  } else {
-    const compiler = Webpack(config);
-    const middleware = WebpackDevMiddleware(compiler, options);
-    // server.app['webpackCompiler'] = compiler;
+exports.register = (server, {config = {}}, next) => {
 
-    server.ext('onRequest', (request, reply) => {
-      middleware(req, res, (err) => {
-        if (err) {
-          return reply(err);
-        }
+  const compiler = Webpack(config);
+  const middleware = WebpackDevMiddleware(compiler, {});
+  server.app['webpackCompiler'] = compiler;
 
-        return reply.continue();
-      });
+  server.ext('onRequest', (request, reply) => {
+    const { req, res } = request.raw;
+
+    middleware(req, res, (err) => {
+      if (err) {
+        reply(err);
+      }
+
+      reply.continue();
     });
+  });
 
-    next();
-  }
+  next();
 };
-
 
 exports.register.attributes = {
   pkg: require('./package.json')
